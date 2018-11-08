8f74f34be907581cbcdfd39f7e65ea593d7f069e
Paul <pwathome@smarterhq.com>
2018-11-08 14:30:31 -0500

    moved nav to its own component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.sass'
-import { push as Menu } from 'react-burger-menu'
-import './Nav.sass'
+import Nav from './components/Nav/Nav'
 
 class App extends Component {
-  showSettings = (event) => {
-    event.preventDefault();
-  }
   render() {
     return (
       <div className="App">
-      <div id="outer-container">
-        <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } right>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="about" className="menu-item" href="/about">About</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a>
-          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-        </Menu>
-        <main id="page-wrap">
-          Main area
-        </main>
-      </div>
+        <div id="outer-container">
+          <Nav />
+          <div id="page-wrap">
+            main content area
+            {/* <aside id="main-image" /> */}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Nav.sass'
+import { push as Menu } from 'react-burger-menu'
+
+export default class Nav extends Component {
+	state = {  }
+	showSettings = (event) => {
+		event.preventDefault();
+	}
+	render() {
+		return (
+			<Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } right>
+				<a id="home" className="menu-item" href="/">Home</a>
+				<a id="about" className="menu-item" href="/about">About</a>
+				<a id="contact" className="menu-item" href="/contact">Contact</a>
+				<a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+		);
+	}
+}
