08f094b2e11e516bc7708e6bae6b38a987f5b2a2
Paul <pwathome@smarterhq.com>
2018-11-17 08:52:35 -0500

    implemented bootstrap. Need to sort out div order

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.sass'
 import Nav from './components/Nav/Nav'
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div id="outer-container">
-          <Nav />
-          <div id="page-wrap">
-            <div id="main-content">
-              main content area
-            </div>
-            <aside id="main-image" />
-          </div>
+            <Nav />
+            <Row id="page-wrap">
+              <Col id="main-content" lg="6" md="6" xs="12">
+                <h1>Main Content Area</h1>
+              </Col>
+
+              <Col id="main-image" lg="6" md="6" xs="12">
+                <img src="https://i2.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?resize=401%2C600&ssl=1" className="img-fluid" width="100%"/>
+              </Col>
+            </Row>
         </div>
       </div>
     );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ export default class Nav extends Component {
 	}
 	render() {
 		return (
-			<Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } right noOverlay>
+			<Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
 				<a id="home" className="menu-item" href="/">Home</a>
 				<a id="about" className="menu-item" href="/about">About</a>
 				<a id="contact" className="menu-item" href="/contact">Contact</a>
