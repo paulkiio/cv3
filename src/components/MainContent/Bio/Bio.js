5e5663d6e28cf639eeec8c7125d86188a4d1d85f
paul <pkwdigital@gmail.com>
2018-11-28 05:45:09 -0500

    added fontawesome

--- src/components/MainContent/Bio/Bio.js
+++ src/components/MainContent/Bio/Bio.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './Bio.sass';
-import { Col, Row, Card } from "reactstrap";
+import { Col, Row } from "reactstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDesktop, faUserMd } from '@fortawesome/free-solid-svg-icons';
 
 export default class Bio extends Component {
 	render() {
+
 		function getCurrentDay () {
 			const today = new Date(),
 						day = today.getDay(),
-						dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"],
-						element = document.getElementById('currentDay');
+						dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+			return dayList[day];
 		}
+
 		return (
 			<Col className="mt-5" sm="12" md="6" md="offset-md-3">
-				<h1>Hi, I'm Paul Wathome.</h1>
+				<h1>Hi, I'm Paul Wathome</h1>
 				<p class="lead mb-3">Developer &amp; Tech Geek</p>
 					<p>
-						Currently based in Indianapolis Indiana. I am passionated about leveraging technology to solve complex problems. When I'm not writing code some of my hobbies include playing soccer, immersing myself in other cultures through travel, DJ-ing and working on my cars. Have look around and have a great <span id="currentDay">day</span> :D
+						Currently based in Indianapolis Indiana. I am passionated about leveraging technology to solve complex problems. When I'm not writing code some of my hobbies include playing soccer, immersing myself in other cultures through travel, DJ-ing and working on my cars. Have look around and have a fantastic <span id="currentDay">{getCurrentDay()}</span> 😉
 					</p>
 
 					<p>
@@ -28,7 +32,7 @@ export default class Bio extends Component {
 				<Row>
 					<Col lg="6" md="6">
 						<div className="media custom-media  d-block">
-							<div className="icon mr-3"><span className="ion-android-cart"></span></div>
+							<div className="mr-3"><FontAwesomeIcon icon={ faDesktop } size="6x" color="#008080"/></div>
 							<div className="media-body">
 								<h3 className="mt-0"><a href="#">Websites</a></h3>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed obcaecati totam.</p>
@@ -37,7 +41,7 @@ export default class Bio extends Component {
 					</Col>
 					<Col lg="6" md="6">
 						<div className="media custom-media  d-block">
-							<div className="icon mr-3"><span className="ion-bonfire"></span></div>
+							<div className="mr-3"><FontAwesomeIcon icon={ faUserMd } size="6x" color="#008080"/></div>
 							<div className="media-body">
 								<h3 className="mt-0"><a href="#">Electronics Repair</a></h3>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed obcaecati, totam.</p>
