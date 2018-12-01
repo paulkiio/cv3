891af01c5e2416bf609968c054bc3aed8cb4dc0d
paul <pkwdigital@gmail.com>
2018-12-01 13:46:09 -0500

    debugging rendering

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -8,7 +8,7 @@ export default class MainContent extends Component {
 	state = {}
 	render () {
 		return (
-			<Col id="main-content" lg={{size: 6, order: 1}} md={{size: 6, order: 1}} sm={{size: 12, order: 2}} xs={{size: 12, order: 2}}>
+			<Col id="main-content" lg={{size: 6}} md={{size: 6}} sm={{size: 12, order: 2}} xs={{size: 12, order: 2}}>
 				{routes.map(route => (
 					<Route
 						key={route.path}
