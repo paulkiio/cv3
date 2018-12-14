6bc227b14350fc6c95cc1b454a7e98f72fedb67f
paul <pkwdigital@gmail.com>
2018-12-14 13:27:13 +0100

    layout portfolio

--- src/components/MainContent/Work/Work.js
+++ src/components/MainContent/Work/Work.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Col } from "reactstrap";
-import './Work.sass'
+import './Work.sass';
+import rsp from '../../../assets/images/rsp.png';
+import KiioLabs from '../../../assets/images/KiioLabs.png';
+import skypoint from '../../../assets/images/skypoint.png';
 
 export default class Work extends Component {
   state = {}
   render () {
-    let placeholder = "https://via.placeholder.com/450";
     return (
       <Col className="mt-5" lg="10" md="10" sm="10" xs={{ size: 10, offset: 1 }}>
         <a href="index.html" className="home-button"><span className="fa fa-home"></span></a>
@@ -14,28 +16,34 @@ export default class Work extends Component {
           <ul className="portfolio-list">
             <li>
               <a href="#">
-                <img src={placeholder} alt="" />
+                <img src={ rsp } alt="" />
                 <div className="text">
-                  <h3>Project Name</h3>
-                  <span>Visit Website</span>
+                  <h3>RennSport Performance</h3>
+                  <span>
+                    Visit Website
+                  </span>
                 </div>
               </a>
             </li>
             <li>
               <a href="#">
-                <img src={placeholder} alt="" />
+                <img src={ KiioLabs } alt="" />
                 <div className="text">
-                  <h3>Project Name</h3>
-                  <span>Visit Website</span>
+                  <h3>Kiio Labs</h3>
+                  <span>
+                    Visit Website
+                  </span>
                 </div>
               </a>
             </li>
             <li>
               <a href="#">
-                <img src={placeholder} alt="" />
+                <img src={ skypoint } alt="" />
                 <div className="text">
-                  <h3>Project Name</h3>
-                  <span>Visit Website</span>
+                  <h3>SkyPoint Ballons</h3>
+                  <span>
+                    Visit Website
+                  </span>
                 </div>
               </a>
             </li>
