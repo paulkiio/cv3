2023b81f723f15705cc549b2513d83b52dedfb47
paul <pkwdigital@gmail.com>
2018-12-15 12:33:31 +0100

    add links to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './Nav.sass';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
+import { FaInstagram } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 export default class Nav extends Component {
 	constructor (props) {
@@ -38,6 +41,11 @@ export default class Nav extends Component {
 						<Link to='/work' onClick={ () => this.closeMenu() }>Work</Link>
 					</li>
 				</ul>
+				<p className='social'>
+					<a href='https://www.instagram.com/the_pkw1/' target='_blank' rel='noopener noreferrer'className='mr-2 mb-2'><FaInstagram size='2em'/></a>
+					<a href='https://github.com/paulkiio' target='_blank' rel='noopener noreferrer' className='mr-2 mb-2'><FaGithub size='2em'/></a>
+					<a href='https://www.linkedin.com/in/paulwathome' target='_blank' rel='noopener noreferrer' className='mr-2 mb-2'><FaLinkedin size='2em'/></a>
+				</p>
 			</Menu>
 		)
 	}
