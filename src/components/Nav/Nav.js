bab0b164d146c8d134a718865f0cd2f8bd7bf25a
paul <pkwdigital@gmail.com>
2018-12-15 01:12:10 +0100

    add auto close funcitonality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import './Nav.sass';
 import { slide as Menu } from 'react-burger-menu';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default class Nav extends Component {
-	state = {}
-	showSettings = (event) => {
-		event.preventDefault();
+	constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
 	}
+
+	handleStateChange (state) {
+		this.setState({ menuOpen: state.isOpen });
+	}
+
+	closeMenu () {
+		this.setState({ menuOpen: false });
+	}
+
 	render () {
 		return (
-			<Menu pageWrapId={'page-wrap'} outerContainerId={'outer-container'} right>
+			<Menu pageWrapId={'page-wrap'}
+						outerContainerId={'outer-container'}
+						right
+						isOpen={ this.state.menuOpen }
+						onStateChange={ (state) => this.handleStateChange(state) }
+			>
 				<ul>
 					<li className='menu-item'>
-						<Link to='/'>Home</Link>
+						<Link to='/' onClick={ () => this.closeMenu() }>Home</Link>
 					</li>
 					<li className='menu-item'>
-						<Link to='/resume'>Resume</Link>
+						<Link to='/resume' onClick={() => this.closeMenu() }>Resume</Link>
 					</li>
 					<li className='menu-item'>
-						<Link to='/work'>Work</Link>
+						<Link to='/work' onClick={ () => this.closeMenu() }>Work</Link>
 					</li>
 					{/* <li onClick={this.showSettings} className='menu-item--small' href=''>
 						Settings
