a26248449e50bcbf2749bdda87234489dc960ea3
Paul Wathome <pkwdigital@gmail.com>
2023-02-06 04:04:49 -0500

    remove nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Row } from 'reactstrap';
-import Nav from './components/Nav/Nav';
-import MainImage from './components/MainImage/MainImage';
-import MainContent from './components/MainContent/MainContent';
+import React, { Component } from "react"
+import "./App.css"
+import { Row } from "reactstrap"
+import MainImage from "./components/MainImage/MainImage"
+import MainContent from "./components/MainContent/MainContent"
 
-
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <div id='outer-container'>
-          <Nav />
-          <Row id='page-wrap'>
-            <MainContent />
-            <MainImage />
-          </Row>
-        </div>
+const App = () => {
+  return (
+    <div className="App">
+      <div id="outer-container">
+        <Row id="page-wrap">
+          <MainContent />
+          <MainImage />
+        </Row>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default App;
+export default App
