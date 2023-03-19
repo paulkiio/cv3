42fb7c2acf9b2290d6c0aa11e02dd70f8e4ef7aa
Paul Wathome <pkwdigital@gmail.com>
2023-03-19 14:41:16 -0400

    clean up nav css and add some color variables

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,12 +4,12 @@ import NavLink from "./NavLink"
 
 const Nav = () => {
   return (
-    <header class="header">
-      <ul class="nav-container">
+    <header className="header">
+      <ul className="nav-container">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/resume">Resume</NavLink>
         <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/">Contact</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </ul>
     </header>
   )
