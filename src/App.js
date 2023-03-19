4e16e9fff052fb780ddb175b3b14efa0edab33c1
Paul Wathome <pkwdigital@gmail.com>
2023-03-19 14:29:27 -0400

    clean up more react strap and add more css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react"
 import "./App.css"
-import { Row } from "reactstrap"
 import MainImage from "./components/MainImage/MainImage"
 import MainContent from "./components/MainContent/MainContent"
 
 const App = () => {
   return (
     <div className="App">
-      <div id="outer-container">
-        <Row id="page-wrap">
-          <MainContent />
-          <MainImage />
-        </Row>
-      </div>
+      <MainContent />
+      <MainImage />
     </div>
   )
 }
