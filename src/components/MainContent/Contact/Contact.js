d7d61e54d8f57304fca383e512e27b45a1f427da
Paul Wathome <pkwdigital@gmail.com>
2023-03-19 14:39:24 -0400

    add contact route

---
+++ src/components/MainContent/Contact/Contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Contact.css"
+
+const Contact = () => {
+  return (
+    <div className="contact-container">Hi from contact</div>
+  )
+}
+
+export default Contact
