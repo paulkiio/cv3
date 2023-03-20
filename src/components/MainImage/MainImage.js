7e86a13c81c47515ecf8895e9767231c4d48c0e9
Paul Wathome <pkwdigital@gmail.com>
2023-03-19 21:43:22 -0400

    refactor mainImage and update css

--- src/components/MainImage/MainImage.js
+++ src/components/MainImage/MainImage.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { Col } from 'reactstrap';
-import './MainImage.css';
+import React from "react"
+import "./MainImage.css"
+import mainImage from "../../assets/images/pkw.png"
 
-export default class MainImage extends Component {
-	state = {}
-	render () {
-		return (
-			<Col id='main-image-container' lg={{size: 6}} md={{size: 6}} sm={{size: 12, order: 1}} xs={{size: 12, order: 1}} />
-		);
-	}
+const MainImage = () => {
+  return (
+    <div id="main-image-container">
+      {/* <img src={mainImage} alt="main" /> */}
+    </div>
+  )
 }
+
+export default MainImage
