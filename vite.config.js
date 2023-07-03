5d299870705f014ea92895322198abba99a39241
pwathome <pwathome@biscuits.local>
2023-07-03 00:07:57 -0400

    convert to JS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [react()],
+  server: {
+    port: 3000
+  }
 })
