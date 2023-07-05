57165e3feefbb3e280ff21bc066efd1bfc818f1b
pwathome <pwathome@biscuits.local>
2023-07-05 09:29:19 -0400

    Update copy

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Working on the Vite + React version of the site,</h1>
+      <h2>Check back for upates soon...</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Last updated {count} days ago...
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
