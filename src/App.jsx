ee1b9075b8ba53cf67ed1221cf21aa9a7dd4f57a
pwathome <pwathome@biscuits.local>
2023-07-07 20:09:04 -0400

    got most of the outer laid out

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import me from './assets/images/pkw.png'
-import viteLogo from '/vite.svg'
+import GitHubLogo from './assets/github.svg'
+import GitLabLogo from './assets/gitlab.svg'
+import LinkedInLogo from './assets/linkedin.svg'
+import Instagram from './assets/instagram.svg'
+import Moon from './assets/icomoon.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <a href="#">
-        <img src={me} className="logo react" alt="React logo" />
-      </a>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <div className="header-buttons-container">
+        {/* <a href="#">LinkedIN</a> */}
+        <div className="socials">
+          <img src={LinkedInLogo} height={20} width={20} />
+          <img src={GitLabLogo} height={20} width={20} />
+          <img src={GitHubLogo} height={20} width={20} />
+          <img src={Instagram} height={20} width={20} />
+        </div>
+
+        <div className="dark-toggle">
+          <img src={Moon} height={20} width={20} />
+        </div>
       </div>
-      <h1>Working on the Vite + React version of the site,</h1>
-      <h2>Check back for upates soon...</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Last updated {count} days ago...
-        </button>
+
+      <div className="heading">
+        <h1>Paul Wathome</h1>
+        <p>
+          I'm baby lo-fi celiac cliche poutine. Letterpress bodega boys flannel, direct trade artisan cupping vexillologist narwhal XOXO affogato YOLO intelligentsia cornhole. Synth succulents chicharrones kitsch keytar tbh, bespoke taiyaki raw denim poke bodega boys. Fit art party wayfarers church-key fingerstache occupy humblebrag pug succulents neutra paleo lyft literally flexitarian adaptogen.
+        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="about">
+        <h2>about.</h2>
+      </div>
+
+      <div className="footer">
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </div>
   )
 }
 
