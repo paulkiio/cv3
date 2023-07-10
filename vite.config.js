fa4dadc3ab75947271794acaf582156c9d6e43a5
pwathome <pwathome@biscuits.local>
2023-07-10 13:50:21 -0400

    vit config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
+    strictPort: true,
+    host: true,
     port: 5173
   },
 })
